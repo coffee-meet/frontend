d8fdea9fd1a05bf1b4a3e03b9bb0839d623a8f81
judahhh <jdh99080516@gmail.com>
2023-10-24 00:51:15 +0900

    style : 전역 디자인 시스템 세팅

---
+++ src/styles/emotion.d.ts
@@ -0,0 +1,9 @@
+import '@emotion/react'
+
+import { type TypeOfPalette, type TypeOfTypo } from './theme'
+declare module '@emotion/react' {
+  export interface Theme {
+    palette: TypeOfPalette
+    typo: TypeOfTypo
+  }
+}
---
+++ src/styles/global.ts
@@ -0,0 +1,125 @@
+import { css } from '@emotion/react'
+import emotionReset from 'emotion-reset'
+
+import { media } from '@/styles/theme'
+export const globalStyle = css`
+  ${emotionReset}
+
+  @font-face {
+    font-family: 'InkLipquid';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_one@1.0/InkLipquid.woff')
+      format('woff');
+    font-weight: normal;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'Pretendard';
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff')
+      format('woff');
+    font-weight: 400;
+    font-style: normal;
+  }
+
+  body {
+    font-family:
+      'Pretendard',
+      Pretendard,
+      -apple-system,
+      BlinkMacSystemFont,
+      system-ui,
+      Roboto,
+      'Helvetica Neue',
+      'Segoe UI',
+      'Apple SD Gothic Neo',
+      'Noto Sans KR',
+      'Malgun Gothic',
+      'Apple Color Emoji',
+      'Segoe UI Emoji',
+      'Segoe UI Symbol',
+      sans-serif !important;
+    box-sizing: border-box;
+    -webkit-tap-highlight-color: transparent;
+    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+    ${media.mobile} {
+      -ms-overflow-style: none;
+    }
+    -ms-overflow-style: none;
+    scrollbar-width: none;
+  }
+  ::-webkit-scrollbar {
+    display: none;
+  }
+  div {
+    box-sizing: border-box;
+  }
+  button {
+    background: inherit;
+    border: none;
+    box-shadow: none;
+    border-radius: 0;
+    padding: 0;
+    overflow: visible;
+    cursor: pointer;
+  }
+  button:focus {
+    outline: none;
+  }
+  input:focus {
+    outline: none;
+  }
+  textarea:focus {
+    outline: none;
+  }
+  .wave {
+    animation: complete 2s;
+    opacity: 0;
+  }
+
+  @keyframes complete {
+    0% {
+      opacity: 1;
+      position: absolute;
+      top: 60%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+    }
+    100% {
+      opacity: 0;
+      position: absolute;
+      top: 60%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+    }
+  }
+
+  .fade-out {
+    opacity: 0;
+    transition: opacity 1s ease-in-out;
+  }
+
+  .is_animating {
+    animation: like 0.5s 1;
+  }
+
+  @keyframes like {
+    0% {
+      transform: scale(1);
+    }
+    90% {
+      transform: scale(1.2);
+    }
+    100% {
+      transform: scale(1.1);
+    }
+  }
+
+  .dark-mode {
+    color: white;
+  }
+  .postTitle {
+    @media (max-width: 375px) {
+      font-size: 20px;
+    }
+  }
+`
