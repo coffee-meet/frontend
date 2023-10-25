42cd08205818ee032a73e341f16e7f9809249365
wukddang <43228743+funkyblues@users.noreply.github.com>
2023-10-25 13:12:47 +0900

    feature:  Divider, Text 공통 컴포넌트 추가

---
+++ src/components/common/Divider/index.tsx
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled'
+
+import { palette } from '@/styles/palette'
+
+export const Divider = styled.div`
+  width: 1px;
+  height: 10px;
+  margin: 0 12px;
+  background-color: ${palette.WHITE};
+`
