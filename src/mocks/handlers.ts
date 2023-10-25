da46f3f602d2f35b588e35697e2d9220f2a885ac
judahhh <jdh99080516@gmail.com>
2023-10-25 23:48:33 +0900

    chore : mock service worker 설치 및 세팅

---
+++ src/mocks/handlers.ts
@@ -0,0 +1,8 @@
+import { http, HttpResponse } from 'msw'
+
+export const handlers = [
+  //example
+  // http.get('/pets', () => {
+  //   return HttpResponse.json(['Tom', 'Jerry', 'Spike'])
+  // }),
+]
