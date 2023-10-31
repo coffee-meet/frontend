f29d741fad3da9fd5b774e6bc0e6d2cceddbc1e2
Changuk Woo <43228743+wukdddang@users.noreply.github.com>
2023-10-31 18:44:33 +0900

    style: PageHeader 컴포넌트 제작 (#59)
    * feature: ExitIcon 컴포넌트 추가
    
    * feature: BackChevron 컴포넌트 추가
    
    * feature: PageHeader 컴포넌트 추가
    
    * refactor: 사용하지 않는 코드 제거
    
    * refactor: tsdoc 추가
    
    * refactor: tsdoc 추가 + exitClick으로 props명 변경
    
    * refactor: PageHeader border-bottom 수정
    
    ---------
    
    Co-authored-by: wukddang <43228743+funkyblues@users.noreply.github.com>

---
+++ src/components/common/BackChevron/index.tsx
@@ -0,0 +1,62 @@
+import styled from '@emotion/styled'
+import { BsChevronLeft } from 'react-icons/bs'
+
+import { palette } from '@/styles/palette'
+
+const StyleBackChevron = styled.div<BackChevronProps>`
+  width: 38px;
+  height: 38px;
+  border-radius: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({ hasBackground, isDarkMode }) =>
+    isDarkMode
+      ? hasBackground
+        ? `${palette.GRAY600}`
+        : 'transparent'
+      : hasBackground
+      ? `${palette.WHITE}`
+      : 'transparent'};
+  border: ${({ hasBackground, isDarkMode }) =>
+    isDarkMode
+      ? hasBackground
+        ? `1px solid ${palette.GRAY500}`
+        : `1px solid ${palette.DARK_TERTIARY}`
+      : hasBackground
+      ? `1px solid ${palette.GRAY300}`
+      : `1px solid ${palette.TERTIARY}`};
+`
+
+type BackChevronProps = {
+  hasBackground?: boolean
+  isDarkMode?: boolean
+  prevClick?: () => void
+}
+
+/**
+ * @param hasBackground - (Optional) 배경색 여부
+ * @param isDarkMode - (Optional) 다크모드 여부
+ * @param prevClick - (Optional) 뒤로가기 클릭 이벤트
+ */
+
+const BackChevron = ({ hasBackground, isDarkMode, prevClick }: BackChevronProps) => {
+  return (
+    <StyleBackChevron hasBackground={hasBackground} isDarkMode={isDarkMode} onClick={prevClick}>
+      <BsChevronLeft
+        size={20}
+        style={{
+          color: isDarkMode
+            ? hasBackground
+              ? `${palette.DARK_WHITE}`
+              : `${palette.DARK_WHITE}`
+            : hasBackground
+            ? `${palette.BLACK}`
+            : `${palette.WHITE}`,
+        }}
+      />
+    </StyleBackChevron>
+  )
+}
+
+export default BackChevron
