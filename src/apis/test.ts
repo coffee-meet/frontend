3bb369487a6c1b0dbec4c49c1d851afddfef908e
Changuk Woo <43228743+wukdddang@users.noreply.github.com>
2023-11-02 18:20:09 +0900

    fix: axios 파일 수정 (#86)
    Co-authored-by: wukddang <43228743+funkyblues@users.noreply.github.com>

--- src/apis/test.ts
+++ src/apis/test.ts
@@ -1,12 +1,5 @@
 import axios from 'axios'
 
 export const testWithBtn = async (nickname: string) => {
-  return axios.get(`http://13.125.194.230/api/v1/users/duplicate`, {
-    headers: {
-      'Referrer-Policy': 'no-referrer', // 또는 필요에 따라 다른 값
-    },
-    params: {
-      nickname,
-    },
-  })
+  return axios.get(`http://13.125.194.230/api/v1/users/duplicate?nickname=${nickname}`)
 }
