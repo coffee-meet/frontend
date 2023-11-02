8d76670799004bbdd30ea5a1b2b5817a0d67032b
wukddang <43228743+funkyblues@users.noreply.github.com>
2023-11-02 18:18:09 +0900

    fix: axios 파일 수정

--- src/pages/profile/ProfileDefault.tsx
+++ src/pages/profile/ProfileDefault.tsx
@@ -1,5 +1,19 @@
+import { useNavigate } from 'react-router-dom'
+
 const ProfileDefault = () => {
-  return <div>{'ProfileDefault'}</div>
+  const navigate = useNavigate()
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          navigate('/profile/edit')
+        }}
+      >
+        {'프로필 수정으로 이동'}
+      </button>
+    </div>
+  )
 }
 
 export default ProfileDefault
