ed037fdabd3d49023c3810cd77d5209385472edc
wukddang <43228743+funkyblues@users.noreply.github.com>
2023-11-04 20:48:03 +0900

    feature: 다크모드/화이트모드 설정을 위한 ThemeStore 추가

---
+++ src/store/ThemeStore.tsx
@@ -0,0 +1,23 @@
+import { create } from 'zustand'
+import { createJSONStorage, persist } from 'zustand/middleware'
+
+type ThemeState = {
+  isDarkMode: boolean
+  storage?: Storage
+  toggleDarkMode: () => void
+}
+
+const useThemeStore = create(
+  persist<ThemeState>(
+    (set) => ({
+      isDarkMode: false,
+      toggleDarkMode: () => set((state) => ({ isDarkMode: !state.isDarkMode })),
+    }),
+    {
+      name: 'theme-store',
+      storage: createJSONStorage(() => localStorage),
+    },
+  ),
+)
+
+export default useThemeStore
