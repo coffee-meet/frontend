c54f4dee35de65de7d3f8e27d492025ec0e0e6b1
wukddang <43228743+funkyblues@users.noreply.github.com>
2023-11-04 20:58:43 +0900

    feature: 다크모드 추가 - GradationBackground, NavigationBar, Home 페이지

--- src/components/common/NavigationBar/index.tsx
+++ src/components/common/NavigationBar/index.tsx
@@ -8,24 +8,39 @@ import Avatar from '@/components/common/Avatar'
 import { FlexBox } from '@/components/common/Flexbox'
 import { palette } from '@/styles/palette'
 import { typo } from '@/styles/typo'
-const NavigationBar = () => {
+
+type NavigationBarProps = {
+  isDarkMode: boolean
+}
+
+const NavigationBar = ({ isDarkMode }: NavigationBarProps) => {
   const navigate = useNavigate()
   const moveFromNavigationBar = (path: string) => {
     navigate(`/${path}`)
   }
   return (
     <StyleWrapper fullWidth={true}>
-      <StyleNavigation justify={'space-around'}>
+      <StyleNavigation isDarkMode={isDarkMode} justify={'space-around'}>
         <StyleNavigationItem onClick={() => moveFromNavigationBar('chat-list')}>
           <FlexBox direction={'column'} gap={5}>
-            <IoChatbox size={'24px'} />
-            <StyleNavigationText>{'이전대화방'}</StyleNavigationText>
+            <IoChatbox
+              size={'24px'}
+              style={{
+                color: isDarkMode ? `${palette.DARK_WHITE}` : `${palette.DARK_BLUE}`,
+              }}
+            />
+            <StyleNavigationText isDarkMode={isDarkMode}>{'이전대화방'}</StyleNavigationText>
           </FlexBox>
         </StyleNavigationItem>
         <StyleNavigationItem onClick={() => moveFromNavigationBar('')}>
           <FlexBox direction={'column'} gap={5}>
-            <MdHome size={'27px'} />
-            <StyleNavigationText>{'홈'}</StyleNavigationText>
+            <MdHome
+              size={'27px'}
+              style={{
+                color: isDarkMode ? `${palette.DARK_WHITE}` : `${palette.DARK_BLUE}`,
+              }}
+            />
+            <StyleNavigationText isDarkMode={isDarkMode}>{'홈'}</StyleNavigationText>
           </FlexBox>
         </StyleNavigationItem>
         <StyleNavigationItem onClick={() => moveFromNavigationBar('profile')}>
@@ -33,7 +48,7 @@ const NavigationBar = () => {
             <StyleProfileImageWrapper>
               <Avatar width={30} height={30} imgUrl={defaultProfileImage} margin={'0'} />
             </StyleProfileImageWrapper>
-            <StyleNavigationText>{'프로필'}</StyleNavigationText>
+            <StyleNavigationText isDarkMode={isDarkMode}>{'프로필'}</StyleNavigationText>
           </FlexBox>
         </StyleNavigationItem>
       </StyleNavigation>
@@ -44,14 +59,18 @@ const StyleWrapper = styled(FlexBox)`
   position: sticky;
   bottom: 0px;
 `
-const StyleNavigationText = styled.span`
-  color: ${palette.GRAY600};
+const StyleNavigationText = styled.span<{
+  isDarkMode: boolean
+}>`
+  color: ${({ isDarkMode }) => (isDarkMode ? palette.DARK_WHITE : palette.DARK_BLUE)};
   font-size: ${typo.Body_10()};
 `
-const StyleNavigation = styled(FlexBox)`
+const StyleNavigation = styled(FlexBox)<{
+  isDarkMode: boolean
+}>`
   width: 100%;
   height: 71px;
-  background-color: white;
+  background-color: ${({ isDarkMode }) => (isDarkMode ? palette.GRAY700 : palette.GRAY100)};
   box-shadow:
     0px 0px 10px 0px rgba(0, 0, 0, 0.24),
     0px 4px 5px 0px rgba(0, 0, 0, 0.14);
