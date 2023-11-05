28bec8044e7db048b5e9947b25044979dfc6552f
wukddang <43228743+funkyblues@users.noreply.github.com>
2023-11-06 00:52:10 +0900

    refactor: Home 레이아웃 수정 - PageContainer 스타일을 동적으로 받도록 수정

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -14,42 +14,46 @@ const Home = () => {
   const [isMatching, setIsMatching] = useState(false)
 
   return (
-    <>
-      <GradationBackground>
-        <AppHeader nickname={nickname} isDarkMode={isDarkMode} />
-        <PageContainer>
-          <Text
-            font={'Body_16'}
-            fontWeight={600}
-            letterSpacing={-0.5}
-            style={{
-              margin: '33px 0 22px 0',
-            }}
-          >
-            {'진행중인 매칭'}
-          </Text>
-          <Card
-            isMatching={isMatching}
-            onClick={() => {
-              setIsMatching((prev) => !prev)
-            }}
-            isDarkMode={isDarkMode}
-          />
-          <Text
-            font={'Body_16'}
-            fontWeight={600}
-            letterSpacing={-0.5}
-            style={{
-              margin: '32px 0 13px 0',
-            }}
-          >
-            {'커피밋의 추천기능'}
-          </Text>
-          <ParticularTopicButton isDarkMode={isDarkMode} />
-        </PageContainer>
-        <NavigationBar />
-      </GradationBackground>
-    </>
+    <GradationBackground>
+      <AppHeader nickname={nickname} isDarkMode={isDarkMode} />
+      <PageContainer
+        height={'80%'}
+        isDarkMode={isDarkMode}
+        style={{
+          padding: '5% 5%',
+        }}
+      >
+        <Text
+          font={'Body_16'}
+          fontWeight={600}
+          letterSpacing={-0.5}
+          style={{
+            margin: '33px 0 22px 0',
+          }}
+        >
+          {'진행중인 매칭'}
+        </Text>
+        <Card
+          isMatching={isMatching}
+          onClick={() => {
+            setIsMatching((prev) => !prev)
+          }}
+          isDarkMode={isDarkMode}
+        />
+        <Text
+          font={'Body_16'}
+          fontWeight={600}
+          letterSpacing={-0.5}
+          style={{
+            margin: '32px 0 13px 0',
+          }}
+        >
+          {'커피밋의 추천기능'}
+        </Text>
+        <ParticularTopicButton isDarkMode={isDarkMode} />
+      </PageContainer>
+      <NavigationBar />
+    </GradationBackground>
   )
 }
 
