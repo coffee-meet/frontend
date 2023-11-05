decd86a127df4d9f36ab5b59775dcc0de0574d28
wukddang <43228743+funkyblues@users.noreply.github.com>
2023-11-06 01:03:09 +0900

    feature: ChatList 페이지 구현 - chatRoomList 더미데이터 추가 - ChatRoomBubbles: chatRoomList를 받아서 ChatRoomBubble 렌더링 - ChatRoomBubble: 채팅방 생성 시점 기준 1일 이후를 D-Day로 설정한 타이머 추가 - max-width: 280px일 때 column이 하나로만 나오도록 반응형 디자인

---
+++ src/components/chatList/ChatRoomBubble.tsx
@@ -0,0 +1,179 @@
+import styled from '@emotion/styled'
+import { timer } from 'd3'
+import { useEffect, useState } from 'react'
+
+import Spacing from '@/components/common/Spacing'
+import { Text, TextWrapper } from '@/components/common/Text'
+import AvatarGroup from '@/components/home/AvatarGroup'
+import { palette } from '@/styles/palette'
+
+const StyledChatRoomBubble = styled.div<{ isDarkMode: boolean }>`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 161px;
+  height: 161px;
+  background-color: ${({ isDarkMode }) => (isDarkMode ? palette.GRAY700 : palette.WHITE)};
+  border-radius: 20px;
+  box-shadow: 0px 4px 20px 0px rgba(0, 0, 0, 0.05);
+  position: relative;
+`
+
+const StyledAvatarGroupWrapper = styled.div`
+  position: relative;
+  width: 100%;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  padding-bottom: 10px;
+`
+
+const StyledChatRoomBubbleTextWrapper = styled(TextWrapper)`
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 0 10px;
+`
+
+const StyledChatRoomBubbleParticipantsWrapper = styled(TextWrapper)`
+  flex-direction: row;
+  justify-content: flex-end;
+  max-width: 80px;
+  flex-wrap: wrap;
+  row-gap: 3px;
+`
+
+type ChatRoomBubbleProps = {
+  title: string
+  participants: string[]
+  createdAt: string
+  isDarkMode: boolean
+}
+
+const ChatRoomBubble = ({ title, participants, createdAt, isDarkMode }: ChatRoomBubbleProps) => {
+  const [timeRemaining, setTimeRemaining] = useState({
+    hours: '00',
+    minutes: '00',
+    seconds: '00',
+  })
+
+  useEffect(() => {
+    const createdTime = new Date(new Date(createdAt).getTime() + 24 * 60 * 60 * 1000)
+
+    const updateTimer = () => {
+      const currentTime = new Date()
+      const timeDifference = createdTime.getTime() - currentTime.getTime()
+
+      if (timeDifference > 0) {
+        const leftHours = String(Math.floor(timeDifference / (1000 * 60 * 60))).padStart(2, '0')
+        const leftMinutes = String(
+          Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60)),
+        ).padStart(2, '0')
+        const leftSeconds = String(Math.floor((timeDifference % (1000 * 60)) / 1000)).padStart(
+          2,
+          '0',
+        )
+        setTimeRemaining({ hours: leftHours, minutes: leftMinutes, seconds: leftSeconds })
+      } else {
+        timerInstance.stop()
+        setTimeRemaining({ hours: '00', minutes: '00', seconds: '00' })
+      }
+    }
+
+    const timerInstance = timer(updateTimer)
+
+    return () => timerInstance.stop()
+  }, [])
+
+  return (
+    <StyledChatRoomBubble isDarkMode={isDarkMode}>
+      <Text
+        font={'Body_16'}
+        fontWeight={600}
+        letterSpacing={-1}
+        style={{
+          color: isDarkMode ? palette.DARK_WHITE : palette.BLACK,
+          padding: '15% 2% 0',
+          textAlign: 'center',
+        }}
+      >
+        {title}
+      </Text>
+      <Spacing size={30} />
+      <AvatarGroup
+        avatarList={['', '', '']}
+        avatarWidth={17}
+        avatarHeight={17}
+        isChatListPage={true}
+        style={{
+          position: 'absolute',
+          top: '70px',
+          right: '10px',
+        }}
+      />
+      <StyledAvatarGroupWrapper>
+        <StyledChatRoomBubbleTextWrapper
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            padding: '0 10px',
+          }}
+        >
+          <Text
+            font={'Body_12'}
+            fontWeight={500}
+            letterSpacing={-1}
+            style={{
+              color: isDarkMode ? palette.DARK_WHITE : palette.GRAY600,
+              display: 'inline-flex',
+            }}
+          >
+            {'참여자'}
+          </Text>
+          <StyledChatRoomBubbleParticipantsWrapper>
+            {participants.map((participant, index) => {
+              return (
+                <Text
+                  key={index}
+                  font={'Body_12'}
+                  fontWeight={500}
+                  letterSpacing={-1}
+                  style={{
+                    color: isDarkMode ? palette.GRAY300 : palette.GRAY500,
+                  }}
+                >
+                  {participant}
+                  {index !== participants.length - 1 && ', '}
+                </Text>
+              )
+            })}
+          </StyledChatRoomBubbleParticipantsWrapper>
+        </StyledChatRoomBubbleTextWrapper>
+        <Spacing size={4} />
+        <StyledChatRoomBubbleTextWrapper>
+          <Text
+            font={'Body_12'}
+            fontWeight={500}
+            letterSpacing={-1}
+            style={{
+              color: isDarkMode ? palette.DARK_WHITE : palette.GRAY600,
+              display: 'inline-flex',
+            }}
+          >
+            {'남은시간'}
+          </Text>
+          <Text
+            font={'Body_12'}
+            fontWeight={500}
+            letterSpacing={-1}
+            style={{
+              color: isDarkMode ? palette.GRAY300 : palette.GRAY500,
+            }}
+          >{`${timeRemaining.hours}시간 ${timeRemaining.minutes}분 ${timeRemaining.seconds}초`}</Text>
+        </StyledChatRoomBubbleTextWrapper>
+      </StyledAvatarGroupWrapper>
+    </StyledChatRoomBubble>
+  )
+}
+
+export default ChatRoomBubble
--- src/pages/chatList/ChatList.tsx
+++ src/pages/chatList/ChatList.tsx
@@ -1,5 +1,73 @@
+import { useNavigate } from 'react-router-dom'
+
+import ChatRoomBubbles from '@/components/chatList/ChatRoomBubbles'
+import BackChevron from '@/components/common/BackChevron'
+import GradationBackground from '@/components/common/GradationBackground'
+import NavigationBar from '@/components/common/NavigationBar'
+import PageContainer from '@/components/common/PageContainer'
+import PageHeader from '@/components/common/PageHeader'
+import Spacing from '@/components/common/Spacing'
+
 const ChatList = () => {
-  return <div>{'ChatList'}</div>
+  // TODO: useThemeStore로 isDarkMode 상태 가져오기
+  const isDarkMode = true
+  const navigate = useNavigate()
+
+  const chatRoomList = [
+    {
+      title: '🥤️ 차가운 아메리카노-6',
+      participants: ['우땅', '빅맘', '롤로노아 조로'],
+      createdAt: '2023-11-05T22:00:00',
+    },
+    {
+      title: '🧃 미지근한 사과주스-23',
+      participants: ['우땅', '빅맘', '루피'],
+      createdAt: '2023-11-05T22:30:00',
+    },
+    {
+      title: '☕️ 따뜻한 아메리카노-10',
+      participants: ['우땅', '빅맘', '나미'],
+      createdAt: '2023-11-05T23:00:00',
+    },
+    {
+      title: '🍰️ 차가운 케이크-8',
+      participants: ['우땅', '빅맘', '상디'],
+      createdAt: '2023-11-05T24:00:00',
+    },
+    {
+      title: '🍦 고소한 아이스크림-2',
+      participants: ['우땅', '빅맘', '우솝'],
+      createdAt: '2023-11-06T00:10:50',
+    },
+  ]
+
+  return (
+    <GradationBackground>
+      <Spacing size={50} />
+      <PageContainer height={'80%'} isDarkMode={isDarkMode}>
+        <PageHeader
+          title={'이전 대화방'}
+          leftIcon={
+            <BackChevron
+              hasBackground={true}
+              isDarkMode={isDarkMode}
+              prevClick={() => {
+                navigate(-1)
+              }}
+            />
+          }
+          isDarkMode={isDarkMode}
+          hasBackground={true}
+          style={{
+            position: 'fixed',
+            zIndex: 1,
+          }}
+        />
+        <ChatRoomBubbles chatRoomList={chatRoomList} isDarkMode={isDarkMode} />
+      </PageContainer>
+      <NavigationBar />
+    </GradationBackground>
+  )
 }
 
 export default ChatList
