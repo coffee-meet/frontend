d663aa2c6b0a46af29bd32f466256e44a9d042d9
judahhh <jdh99080516@gmail.com>
2023-11-07 14:49:04 +0900

    fix : 에러 주석 처리

--- src/components/messageArea/index.tsx
+++ src/components/messageArea/index.tsx
@@ -1,16 +1,16 @@
-import { Messages } from '@/apis/chatting/chattingType'
+// import { Messages } from '@/apis/chatting/chattingType'
 import { FlexBox } from '@/components/common/Flexbox'
-import AdminListRow from '@/components/common/ListRow/AdminListRow'
+// import AdminListRow from '@/components/common/ListRow/AdminListRow'
 import Spacing from '@/components/common/Spacing'
 
-interface MessageProps {
-  messageData: Messages[]
-}
-const MessageArea = ({ messageData }: MessageProps) => {
+// interface MessageProps {
+//   messageData: Messages[]
+// }
+const MessageArea = () => {
   return (
     <>
       <FlexBox direction={'column'} gap={20} fullWidth={true}>
-        <Spacing size={20}></Spacing>
+        <Spacing size={20} />
       </FlexBox>
     </>
   )
