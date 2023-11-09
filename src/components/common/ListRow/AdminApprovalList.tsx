562bfff4577dce8e2e678cc7083df65dc3db2a5f
from1to2 <ngcd12345@gmail.com>
2023-11-09 23:28:18 +0900

    feat: 승인페이지 mock data 적용. 목록 클릭 함수 추가. interface 추가.

--- src/components/common/ListRow/AdminApprovalList.tsx
+++ src/components/common/ListRow/AdminApprovalList.tsx
@@ -3,81 +3,40 @@ import styled from '@emotion/styled'
 import AdminApprovalListRow from '@/components/common/ListRow/AdminApprovalListRow'
 import { palette } from '@/styles/palette'
 
-const AdminApprovalList = () => {
+interface AdminApprovalListProps {
+  onApproveSelect: (nickname: string) => void
+}
+
+const AdminApprovalList = ({ onApproveSelect }: AdminApprovalListProps) => {
+  const handlePersonApproval = (nickname: string) => {
+    onApproveSelect(nickname)
+  }
+  const AdminApprovalListData = [
+    { nickname: '유명한', height: 60, infoMessage: '대기 중', isDarkMode: false },
+    { nickname: '박상민', height: 60, infoMessage: '대기 중', isDarkMode: false },
+    { nickname: '박은지', height: 60, infoMessage: '대기 중', isDarkMode: false },
+    { nickname: '주다현', height: 60, infoMessage: '대기 중', isDarkMode: false },
+    { nickname: '남궁호수', height: 60, infoMessage: '대기 중', isDarkMode: false },
+    { nickname: '우창욱', height: 60, infoMessage: '대기 중', isDarkMode: false },
+    { nickname: '홍길동', height: 60, infoMessage: '대기 중', isDarkMode: false },
+    { nickname: '홍길동', height: 60, infoMessage: '대기 중', isDarkMode: false },
+    { nickname: '홍길동', height: 60, infoMessage: '대기 중', isDarkMode: false },
+    { nickname: '홍길동', height: 60, infoMessage: '대기 중', isDarkMode: false },
+    { nickname: '홍길동', height: 60, infoMessage: '대기 중', isDarkMode: false },
+    { nickname: '홍길동', height: 60, infoMessage: '대기 중', isDarkMode: false },
+    { nickname: '홍길동', height: 60, infoMessage: '대기 중', isDarkMode: false },
+    { nickname: '홍길동', height: 60, infoMessage: '대기 중', isDarkMode: false },
+  ]
+
   return (
     <AdminApprovalListContainer>
-      <AdminApprovalListRow
-        nickname={'박상민'}
-        height={60}
-        infoMessage={'대기 중'}
-        isDarkMode={false}
-      />
-      <AdminApprovalListRow
-        nickname={'유명한'}
-        height={60}
-        infoMessage={'대기 중'}
-        isDarkMode={false}
-      />
-      <AdminApprovalListRow
-        nickname={'박은지'}
-        height={60}
-        infoMessage={'대기 중'}
-        isDarkMode={false}
-      />
-      <AdminApprovalListRow
-        nickname={'주다현'}
-        height={60}
-        infoMessage={'대기 중'}
-        isDarkMode={false}
-      />
-      <AdminApprovalListRow
-        nickname={'우창욱'}
-        height={60}
-        infoMessage={'대기 중'}
-        isDarkMode={false}
-      />
-      <AdminApprovalListRow
-        nickname={'남궁호수'}
-        height={60}
-        infoMessage={'대기 중'}
-        isDarkMode={false}
-      />
-      <AdminApprovalListRow
-        nickname={'홍길동'}
-        height={60}
-        infoMessage={'대기 중'}
-        isDarkMode={false}
-      />
-      <AdminApprovalListRow
-        nickname={'홍길동'}
-        height={60}
-        infoMessage={'대기 중'}
-        isDarkMode={false}
-      />
-      <AdminApprovalListRow
-        nickname={'홍길동'}
-        height={60}
-        infoMessage={'대기 중'}
-        isDarkMode={false}
-      />
-      <AdminApprovalListRow
-        nickname={'홍길동'}
-        height={60}
-        infoMessage={'대기 중'}
-        isDarkMode={false}
-      />
-      <AdminApprovalListRow
-        nickname={'홍길동'}
-        height={60}
-        infoMessage={'대기 중'}
-        isDarkMode={false}
-      />
-      <AdminApprovalListRow
-        nickname={'홍길동'}
-        height={60}
-        infoMessage={'대기 중'}
-        isDarkMode={false}
-      />
+      {AdminApprovalListData.map((data, index) => (
+        <AdminApprovalListRow
+          key={index}
+          {...data}
+          onClick={() => handlePersonApproval(data.nickname)}
+        />
+      ))}
     </AdminApprovalListContainer>
   )
 }
