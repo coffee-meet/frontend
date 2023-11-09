81425edf6130602452fb3ed7ba4981c914ab1213
from1to2 <ngcd12345@gmail.com>
2023-11-09 22:01:34 +0900

    feat: AdminReportList mockdata 추가, 함수 연결

--- src/components/common/ListRow/AdminReportList.tsx
+++ src/components/common/ListRow/AdminReportList.tsx
@@ -2,86 +2,43 @@ import styled from '@emotion/styled'
 
 import AdminReportListRow from '@/components/common/ListRow/AdminReportListRow'
 import { palette } from '@/styles/palette'
+interface AdminReportListProps {
+  onSelect: (nickname: string) => void
+}
+const AdminReportList = ({ onSelect }: AdminReportListProps) => {
+  const handlePersonReported = (nickname: string) => {
+    onSelect(nickname)
+  }
+  const AdminReportListData = [
+    { nickname: '유명한', height: 60, infoMessage: '누적 1회', isDarkMode: false },
+    { nickname: '박상민', height: 60, infoMessage: '누적 2회', isDarkMode: false },
+    { nickname: '박은지', height: 60, infoMessage: '누적 2회', isDarkMode: false },
+    { nickname: '주다현', height: 60, infoMessage: '누적 1회', isDarkMode: false },
+    { nickname: '남궁호수', height: 60, infoMessage: '누적 1회', isDarkMode: false },
+    { nickname: '우창욱', height: 60, infoMessage: '누적 2회', isDarkMode: false },
+    { nickname: '홍길동', height: 60, infoMessage: '누적 1회', isDarkMode: false },
+    { nickname: '홍길동', height: 60, infoMessage: '누적 1회', isDarkMode: false },
+    { nickname: '홍길동', height: 60, infoMessage: '누적 1회', isDarkMode: false },
+    { nickname: '홍길동', height: 60, infoMessage: '누적 1회', isDarkMode: false },
+    { nickname: '홍길동', height: 60, infoMessage: '누적 1회', isDarkMode: false },
+    { nickname: '홍길동', height: 60, infoMessage: '누적 1회', isDarkMode: false },
+    { nickname: '홍길동', height: 60, infoMessage: '누적 1회', isDarkMode: false },
+    { nickname: '홍길동', height: 60, infoMessage: '누적 1회', isDarkMode: false },
+  ]
 
-const AdminReportList = () => {
   return (
-    <AdminReportListContainer>
-      <AdminReportListRow
-        nickname={'유명한'}
-        height={60}
-        infoMessage={'누적 1회'}
-        isDarkMode={false}
-      />
-      <AdminReportListRow
-        nickname={'박상민'}
-        height={60}
-        infoMessage={'누적 2회'}
-        isDarkMode={false}
-      />
-      <AdminReportListRow
-        nickname={'박은지'}
-        height={60}
-        infoMessage={'누적 2회'}
-        isDarkMode={false}
-      />
-      <AdminReportListRow
-        nickname={'주다현'}
-        height={60}
-        infoMessage={'누적 1회'}
-        isDarkMode={false}
-      />
-      <AdminReportListRow
-        nickname={'남궁호수'}
-        height={60}
-        infoMessage={'누적 1회'}
-        isDarkMode={false}
-      />
-      <AdminReportListRow
-        nickname={'우창욱'}
-        height={60}
-        infoMessage={'누적 2회'}
-        isDarkMode={false}
-      />
-      <AdminReportListRow
-        nickname={'홍길동'}
-        height={60}
-        infoMessage={'누적 2회'}
-        isDarkMode={false}
-      />
-      <AdminReportListRow
-        nickname={'홍길동'}
-        height={60}
-        infoMessage={'누적 3회'}
-        isDarkMode={false}
-      />
-      <AdminReportListRow
-        nickname={'홍길동'}
-        height={60}
-        infoMessage={'누적 2회'}
-        isDarkMode={false}
-      />
-      <AdminReportListRow
-        nickname={'홍길동'}
-        height={60}
-        infoMessage={'누적 2회'}
-        isDarkMode={false}
-      />
-      <AdminReportListRow
-        nickname={'홍길동'}
-        height={60}
-        infoMessage={'누적 1회'}
-        isDarkMode={false}
-      />
-      <AdminReportListRow
-        nickname={'홍길동'}
-        height={60}
-        infoMessage={'누적 2회'}
-        isDarkMode={false}
-      />
-    </AdminReportListContainer>
+    <StyledAdminReportListContainer>
+      {AdminReportListData.map((data, index) => (
+        <AdminReportListRow
+          key={index}
+          {...data}
+          onClick={() => handlePersonReported(data.nickname)}
+        />
+      ))}
+    </StyledAdminReportListContainer>
   )
 }
-const AdminReportListContainer = styled.div`
+const StyledAdminReportListContainer = styled.div`
   background-color: ${palette.WHITE};
   overflow: scroll;
   height: 500px;
