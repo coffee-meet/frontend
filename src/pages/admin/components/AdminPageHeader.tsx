71b7c34c509d9b1c42b0bd21cff9d6a6bf4afd78
from1to2 <ngcd12345@gmail.com>
2023-11-11 21:35:11 +0900

    feat: AdminPageHeader 컴포넌트 추가

---
+++ src/pages/admin/components/AdminPageHeader.tsx
@@ -0,0 +1,62 @@
+import styled from '@emotion/styled'
+import { BiLeftArrowAlt } from 'react-icons/bi'
+
+import { Text } from '@/components/common/Text'
+import { palette } from '@/styles/palette'
+
+interface AdminPageHeaderProps {
+  username: string
+  // onBack: () => void;
+  //onclick 함수 보류
+}
+
+const AdminPageHeader = ({ username }: AdminPageHeaderProps) => {
+  return (
+    <StyledAdminPageHeaderContainerOuterWrapper>
+      <StyledAdminPageHeaderContainer>
+        <StyledBackIcon>
+          <BiLeftArrowAlt
+            size={'30px'}
+            style={{
+              color: palette.GRAY600,
+              cursor: 'pointer',
+            }}
+          />
+        </StyledBackIcon>
+        <Text font={'Body_18'} fontWeight={900} letterSpacing={-1}>
+          <StyledUsernameWrapper>{username}</StyledUsernameWrapper>
+        </Text>
+
+        <StyledBackIconWidthRightSpace />
+      </StyledAdminPageHeaderContainer>
+    </StyledAdminPageHeaderContainerOuterWrapper>
+  )
+}
+const StyledAdminPageHeaderContainerOuterWrapper = styled.div`
+  background-color: ${palette.WHITE};
+`
+const StyledAdminPageHeaderContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 93%;
+  margin: 0 auto;
+  height: 70px;
+  background-color: ${palette.WHITE};
+  border-bottom: 1px solid ${palette.GRAY300};
+`
+
+const StyledBackIcon = styled.span`
+  margin-left: 20px;
+  cursor: pointer;
+`
+
+const StyledUsernameWrapper = styled.span`
+  margin: 0;
+`
+
+const StyledBackIconWidthRightSpace = styled.div`
+  width: 40px;
+`
+
+export default AdminPageHeader
