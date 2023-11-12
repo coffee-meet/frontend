06fb8689c00f573f5d515c24fe6d201f3ee159a1
judahhh <jdh99080516@gmail.com>
2023-11-12 18:38:04 +0900

    chore : msw 버전에 따른 import 수정

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,14 +1,15 @@
-// import { rest } from 'msw'
+import { rest } from 'msw'
+const nickname = '주다다'
 
-// export const handlers = [
-//   // example
-//   rest.get(`/v1/users/duplicate?nickname=${nickname}`, async (req, res, ctx) => {
-//     await sleep(200)
-//     return res(ctx.status(200), ctx.json())
-//   }),
-// ]
-// async function sleep(timeout: number) {
-//   return new Promise((resolve) => {
-//     setTimeout(resolve, timeout)
-//   })
-// }
+export const handlers = [
+  rest.get(`/v1/users/duplicate?nickname=${nickname}`, async (req, res, ctx) => {
+    await sleep(200)
+    return res(ctx.status(200), ctx.json(res))
+  }),
+]
+
+async function sleep(timeout: number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, timeout)
+  })
+}
