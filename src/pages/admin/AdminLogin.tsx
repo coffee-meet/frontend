2def4a612d8395c5be9e36220112a0c66746e231
from1to2 <ngcd12345@gmail.com>
2023-11-17 18:19:16 +0900

    feat: 관리자 로그인 페이지 - 피그마 디자인, 구현

--- src/pages/admin/AdminLogin.tsx
+++ src/pages/admin/AdminLogin.tsx
@@ -1,8 +1,12 @@
+import styled from '@emotion/styled'
 import { useMutation } from '@tanstack/react-query'
 import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 
 import AdminLoginAPI from '@/apis/adminLogin/AdminLoginApi'
+import NormalButton from '@/components/common/Buttons/NormalButton'
+import Spacing from '@/components/common/Spacing'
+import { palette } from '@/styles/palette'
 
 const AdminLogin = () => {
   // form 제출 -> API 요청 -> 결과값에 따라 페이지 이동
@@ -27,11 +31,57 @@ const AdminLogin = () => {
         onSubmitAdminLoginData(adminData)
       })}
     >
-      <input {...register('adminId')} placeholder={'Admin ID'} />
-      <input {...register('adminPassword')} placeholder={'Admin Password'} />
+      <Spacing size={56}></Spacing>
+      <StyledInputWrapper>
+        <input
+          style={{
+            width: '351px',
+            height: '46px',
+            border: '1px solid #000',
+            borderRadius: '10px',
+            marginBottom: '10px',
+            boxSizing: 'border-box',
+            padding: '0 0 0 10px',
+            borderColor: palette.GRAY300,
+          }}
+          {...register('adminId')}
+          placeholder={'관리자 아이디'}
+        />
+      </StyledInputWrapper>
+      <Spacing size={43}></Spacing>
+
+      <StyledInputWrapper>
+        <input
+          style={{
+            width: '351px',
+            height: '46px',
+            border: '1px solid #000',
+            borderRadius: '10px',
+            marginBottom: '10px',
+            boxSizing: 'border-box',
+            padding: '0 0 0 10px',
+            borderColor: palette.GRAY300,
+          }}
+          {...register('adminPassword')}
+          placeholder={'관리자 비밀번호'}
+        />
+      </StyledInputWrapper>
+
       <p>{adminData}</p>
-      <input type={'submit'} />
+      <Spacing size={519}></Spacing>
+
+      <StyledBtnWrapper>
+        <NormalButton type={'submit'} normalButtonType={'form-submit'}>
+          {'관리자 로그인'}
+        </NormalButton>
+      </StyledBtnWrapper>
     </form>
   )
 }
+const StyledInputWrapper = styled.div`
+  text-align: center;
+`
+const StyledBtnWrapper = styled.div`
+  text-align: center;
+`
 export default AdminLogin
