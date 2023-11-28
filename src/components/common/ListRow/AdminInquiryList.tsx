9d3c33b10e734776b87b3816d08f26207197c731
from1to2 <ngcd12345@gmail.com>
2023-11-28 10:31:56 +0900

    feat: 문의,신고 리스트 배포환경 mock data 추가

--- src/components/common/ListRow/AdminInquiryList.tsx
+++ src/components/common/ListRow/AdminInquiryList.tsx
@@ -3,36 +3,63 @@ import { useQuery } from '@tanstack/react-query'
 
 import AdminInquiryAPI from '@/apis/adminInquiry/AdminInquiryApi'
 import AdminApprovalListRow from '@/components/common/ListRow/AdminApprovalListRow'
-import AdminPageHeader from '@/pages/admin/components/AdminPageHeader'
 import { palette } from '@/styles/palette'
 
 interface AdminInquiryListProps {
   onInquirySelectUserName: (nickname: string) => void
 }
-interface InquiryListData {
-  inquiryRequestUser: string
-  inquiryRequestDate: string
-  onInquirySelectUserName: (nickname: string) => void
-}
-interface InquiryListData {
-  approvalRequestUser: string
-  approvalRequestUserStatus: string
-}
+// interface InquiryListData {
+//   inquiryRequestUser: string
+//   inquiryRequestDate: string
+//   onInquirySelectUserName: (nickname: string) => void
+// }
+// interface InquiryListData {
+//   approvalRequestUser: string
+//   approvalRequestUserStatus: string
+// }
 
 const AdminInquiryList = ({ onInquirySelectUserName }: AdminInquiryListProps) => {
   // API 요청 코드
-  const { data, isSuccess } = useQuery(['AdminInquiryList'], AdminInquiryAPI.GET_INQUIRY_LIST)
+  const { data, isSuccess, isError } = useQuery(
+    ['AdminInquiryList'],
+    AdminInquiryAPI.GET_INQUIRY_LIST,
+  )
 
   const handlePersonInquiry = (inquiryNickname: string) => {
     onInquirySelectUserName(inquiryNickname)
   }
-  const inquiryDatas = data?.data.inquiries
+  console.log(isSuccess && data)
+  console.log(isError)
+  // const inquiryDatas = data?.data.inquiries
+
+  const mockData = [
+    { inquiryRequestUser: '박상민', inquiryRequestDate: '2023.11.22' },
+    { inquiryRequestUser: '박은지', inquiryRequestDate: '2023.11.22' },
+    { inquiryRequestUser: '주다현', inquiryRequestDate: '2023.11.22' },
+    { inquiryRequestUser: '남궁호수', inquiryRequestDate: '2023.11.21' },
+    { inquiryRequestUser: '유명한', inquiryRequestDate: '2023.11.21' },
+    { inquiryRequestUser: '박상민', inquiryRequestDate: '2023.11.17' },
+    { inquiryRequestUser: '남궁호수', inquiryRequestDate: '2023.11.17' },
+    { inquiryRequestUser: '주다현', inquiryRequestDate: '2023.11.17' },
+    { inquiryRequestUser: '박은지', inquiryRequestDate: '2023.11.17' },
+    { inquiryRequestUser: '우창욱', inquiryRequestDate: '2023.11.10' },
+    { inquiryRequestUser: '유명한', inquiryRequestDate: '2023.11.10' },
+  ]
 
   return (
     <AdminInquiryListContainerOuterWrapper>
-      <AdminPageHeader username={'문의 사항 처리'}></AdminPageHeader>
       <AdminInquiryListContainer>
-        {isSuccess &&
+        {mockData.map((data, index) => (
+          <AdminApprovalListRow
+            key={index}
+            height={71}
+            nickname={data.inquiryRequestUser}
+            infoMessage={data.inquiryRequestDate}
+            isDarkMode={false}
+            onClick={() => handlePersonInquiry(data.inquiryRequestUser)}
+          />
+        ))}
+        {/* {isSuccess &&
           inquiryDatas.map((inquiryListData: InquiryListData, index: number) => (
             <AdminApprovalListRow
               key={index}
@@ -42,7 +69,7 @@ const AdminInquiryList = ({ onInquirySelectUserName }: AdminInquiryListProps) =>
               isDarkMode={false}
               onClick={() => handlePersonInquiry(inquiryListData.inquiryRequestUser)}
             />
-          ))}
+          ))} */}
       </AdminInquiryListContainer>
     </AdminInquiryListContainerOuterWrapper>
   )
