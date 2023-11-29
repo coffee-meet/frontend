74f76e29e280b0d6b01257b44590b2decea92563
sscoderati <chungup5495@gmail.com>
2023-11-29 10:31:32 +0900

    [Refactor]: 닉네임 중복확인 함수 및 관심사 목록 상수 분리

---
+++ src/apis/register/getNicknameValid.ts
@@ -0,0 +1,15 @@
+import { axiosAPI } from '@/apis/axios.ts'
+
+const getNicknameValid = async (nickname: string) => {
+  try {
+    const res = await axiosAPI.get(`/v1/users/duplicate?nickname=${nickname}`)
+    if (res.status !== 200) {
+      throw new Error('nickname valid check failed!')
+    }
+  } catch (err) {
+    console.log(err)
+    throw err
+  }
+}
+
+export default getNicknameValid
