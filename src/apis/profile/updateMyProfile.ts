be113f0285e1ccab53b7db815d8e59cf8dc01180
sscoderati <chungup5495@gmail.com>
2023-11-30 02:38:40 +0900

    [Feature]: 내 프로필 수정 api 요청 함수 추가

---
+++ src/apis/profile/updateMyProfile.ts
@@ -0,0 +1,16 @@
+import { axiosAPI } from '@/apis/axios.ts'
+import { MyProfileUpdateRequest } from '@/apis/profile/type.ts'
+
+const updateMyProfile = async (data: MyProfileUpdateRequest) => {
+  try {
+    const res = await axiosAPI.patch('/v1/users/me', data)
+    if (res.status !== 200) {
+      throw new Error('update my profile failed!')
+    }
+  } catch (err) {
+    console.log(err)
+    throw err
+  }
+}
+
+export default updateMyProfile
