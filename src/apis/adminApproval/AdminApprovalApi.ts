68693d4f75001773aa8a65d4f293374f40bf670c
from1to2 <ngcd12345@gmail.com>
2023-12-01 16:40:33 +0900

    feat: approval API

--- src/apis/adminApproval/AdminApprovalApi.ts
+++ src/apis/adminApproval/AdminApprovalApi.ts
@@ -1,33 +1,48 @@
 import { axiosAPI } from '@/apis/axios'
 
+// interface ApprovalRequestList {
+//   certificationId: number
+//   nickname: string
+//   companyName: string
+//   companyEmail: string
+//   businessCardUrl: string
+//   department: string
+// }
+const GET_APPROVAL_LIST_PARAMS = {
+  offset: '0',
+  size: '10',
+}
+
 const AdminApprovalAPI = {
   GET_APPROVAL_REQUEST_LIST: async () => {
-    const response = await axiosAPI.get(`/v1/users/approvals`)
+    const response = await axiosAPI.get(`/v1/admins/certifications/pending`, {
+      params: GET_APPROVAL_LIST_PARAMS,
+    })
     return {
       data: response.data,
     }
   },
   GET_APPROVAL_INFO: async () => {
-    const response = await axiosAPI.get(`/v1/useres/inquries/:inquryId`)
+    const response = await axiosAPI.get(`/v1/admins/certifications/pending`, {
+      params: GET_APPROVAL_LIST_PARAMS,
+    })
     return {
       data: response.data,
     }
   },
-  POST_APPROVAL_ACCEPT: async () => {
-    const response = await axiosAPI.post(
-      '/v1/certification/users/:userId/accept',
-      { decision: 'accept' },
-      {
-        headers: {
-          'Content-Type': 'application/json',
-        },
+  // certificationId: string
+  // ${certificationId}
+  POST_APPROVAL_ACCEPT: async (certificationId: string) => {
+    const response = await axiosAPI.patch(`/v1/admins/certifications/${certificationId}/approval`, {
+      headers: {
+        'Content-Type': 'application/json',
       },
-    )
+    })
     return response.data
   },
-  POST_APPROVAL_REJECT: async () => {
+  POST_APPROVAL_REJECT: async (certificationId: string) => {
     const response = await axiosAPI.post(
-      '/v1/certification/users/:userId/reject',
+      `/v1/admins/certifications/${certificationId}/rejection`,
       { decision: 'reject' },
       {
         headers: {
