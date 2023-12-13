349c4284b4baf0335e20790947c349f1abd0ee61
sscoderati <chungup5495@gmail.com>
2023-12-13 22:14:12 +0900

    Chore: ESLint 룰 재정의

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,71 +3,82 @@ module.exports = {
     browser: true,
     es2021: true,
   },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-    'plugin:react/recommended',
-    'prettier',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "plugin:react/recommended",
+    "prettier",
   ],
-  plugins: ['react', 'prettier', '@typescript-eslint', 'simple-import-sort'],
+  plugins: ["react", "prettier", "@typescript-eslint"],
   overrides: [
     {
       env: {
         node: true,
       },
-      files: ['.eslintrc.{js,cjs}'],
+      files: [".eslintrc.{js,cjs}"],
       parserOptions: {
-        sourceType: 'script',
+        sourceType: "script",
       },
     },
   ],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
   rules: {
     // eslint-disable-next-line @typescript-eslint/no-var-requires
-    'linebreak-style': ['error', require('os').EOL === '\r\n' ? 'windows' : 'unix'],
-    'prettier/prettier': ['error', { endOfLine: 'auto' }],
-    'react/no-unknown-property': ['error', { ignore: ['css'] }],
-    'react/prop-types': 'off',
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': 'off',
-    '@typescript-eslint/no-use-before-define': 'off',
-    '@typescript-eslint/strict-boolean-expressions': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    'simple-import-sort/imports': 'error',
-    'simple-import-sort/exports': 'error',
-    'react/react-in-jsx-scope': 'off',
-    'react/jsx-curly-brace-presence': ['error', { props: 'always', children: 'always' }],
-    'react/function-component-definition': [
-      'error',
+    "linebreak-style": ["error", require("os").EOL === "\r\n" ? "windows" : "unix"],
+    "prettier/prettier": ["error", { endOfLine: "auto" }],
+    "react/no-unknown-property": ["error", { ignore: ["css"] }],
+    "react/prop-types": "off",
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-use-before-define": "off",
+    "@typescript-eslint/strict-boolean-expressions": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "react/react-in-jsx-scope": "off",
+    curly: "error",
+    "react/jsx-curly-brace-presence": ["error", { props: "always", children: "always" }],
+    "react/function-component-definition": [
+      "error",
       {
-        namedComponents: 'arrow-function',
-        unnamedComponents: 'arrow-function',
+        namedComponents: "arrow-function",
+        unnamedComponents: "arrow-function",
       },
     ],
-    '@typescript-eslint/naming-convention': [
-      'error',
+    "@typescript-eslint/naming-convention": [
+      "error",
       {
-        format: ['camelCase', 'UPPER_CASE', 'PascalCase'],
-        selector: 'variable',
-        leadingUnderscore: 'allow',
+        format: ["camelCase", "UPPER_CASE", "PascalCase"],
+        selector: "variable",
+        leadingUnderscore: "allow",
       },
       {
-        format: ['camelCase', 'PascalCase'],
-        selector: 'function',
+        format: ["camelCase", "PascalCase"],
+        selector: "function",
       },
       {
-        format: ['PascalCase'],
-        selector: 'interface',
+        format: ["PascalCase"],
+        selector: "interface",
       },
       {
-        format: ['PascalCase'],
-        selector: 'typeAlias',
+        format: ["PascalCase"],
+        selector: "typeAlias",
+      },
+    ],
+    "@typescript-eslint/consistent-type-imports": [
+      "error",
+      {
+        prefer: "type-imports",
+        disallowTypeAnnotations: false,
       },
     ],
   },
-  parser: '@typescript-eslint/parser',
-}
+  parser: "@typescript-eslint/parser",
+};
