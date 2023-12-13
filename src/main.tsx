4e1d1228f8b7df29b592717585f8a52cb79b890d
sscoderati <chungup5495@gmail.com>
2023-12-13 22:08:26 +0900

    Remove: 필요없는 파일 삭제

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,15 @@
-import { Global, ThemeProvider } from '@emotion/react'
-import { QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Global, ThemeProvider } from "@emotion/react";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { queryClient } from "@/apis/queryClient";
+import { globalStyle } from "@/styles/index.tsx";
+import { theme } from "@/styles/index.tsx";
+import App from "./App.tsx";
 
-import { queryClient } from '@/apis/queryClient'
-import { worker } from '@/mocks/worker'
-import { globalStyle } from '@/styles/index.tsx'
-import { theme } from '@/styles/index.tsx'
-
-import App from './App.tsx'
-
-if (process.env.NODE_ENV === 'development') {
-  worker.start()
-}
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter basename={'/'}>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <BrowserRouter basename={"/"}>
     <Global styles={globalStyle} />
     <ThemeProvider theme={theme}>
       <QueryClientProvider client={queryClient}>
@@ -25,4 +18,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       </QueryClientProvider>
     </ThemeProvider>
   </BrowserRouter>,
-)
+);
