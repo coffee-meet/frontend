f89f8b12ecb11c51552198d7632a9bb826b4286a
sscoderati <chungup5495@gmail.com>
2024-01-03 15:59:10 +0900

    Chore: UserInfoSchema에서 userId 제거

--- src/schemas/userInfo.ts
+++ src/schemas/userInfo.ts
@@ -1,7 +1,6 @@
 import * as z from "zod";
 
 export const UserInfoSchema = z.object({
-  userId: z.string(),
   nickname: z.string().min(1, { message: "닉네임을 입력해주세요." }),
   interest: z.array(z.string().min(1, { message: "관심사를 선택해주세요." })),
 });
