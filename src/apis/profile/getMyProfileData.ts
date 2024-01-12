882cf0af50d485c4a07780dfcb2505dc12c7de6a
sscoderati <chungup5495@gmail.com>
2024-01-12 18:58:07 +0900

    Chore: getMyProfileData 수정
    - Error 객체 throwing 로직 삭제

--- src/apis/profile/getMyProfileData.ts
+++ src/apis/profile/getMyProfileData.ts
@@ -4,9 +4,6 @@ import type { MyProfileData } from "@/apis/profile/type.ts";
 const getMyProfileData = async () => {
   try {
     const res = await axiosAPI.get<MyProfileData>("/v1/users/me");
-    if (res.status !== 200) {
-      throw new Error("get my profile data failed!");
-    }
     return res.data;
   } catch (err) {
     console.log(err);
