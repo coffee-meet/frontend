f38c821519ecc41cc61cd63adaad1e3f8a8f7faa
sscoderati <chungup5495@gmail.com>
2024-01-12 19:13:26 +0900

    Refactor: axiosAPI 미들웨어 비활성화
    - 요청 미들웨어에서 액세스 토큰값 추가하는 로직은 headers 옵션으로 직접 설정

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -15,40 +15,9 @@ export const axiosAPI = axios.create({
     process.env.NODE_ENV === "development"
       ? "http://localhost:5173"
       : import.meta.env.VITE_BASE_URL,
+  headers: {
+    Authorization: `${state.authTokens.accessToken}`,
+  },
 });
 
-// Add a request interceptor
-axiosAPI.interceptors.request.use(
-  function (config) {
-    config.headers["Authorization"] = `${state.authTokens.accessToken}`;
-
-    return config;
-  },
-  function (error) {
-    return Promise.reject(error);
-  },
-);
-
-// Add a response interceptor
-axiosAPI.interceptors.response.use(
-  function (response) {
-    /*
-        http status가 200인 경우
-        응답 바로 직전에 대해 작성합니다.
-        .then() 으로 이어집니다.
-    */
-    return response;
-  },
-
-  function (error) {
-    /*
-        http status가 200이 아닌 경우
-        응답 에러 처리를 작성합니다.
-        .catch() 으로 이어집니다.
-    */
-    if (error.response && error.response.status === 401) {
-      return new Promise(() => {}); //이행되지 않은 Promise를 반환하여 Promise Chaining 끊어주기
-    }
-    return Promise.reject(error);
-  },
-);
+// TODO: axiosAPI.interceptors.response.use()를 사용하여 refreshToken이 만료되었을 때, accessToken을 재발급 받는 로직을 구현해야 합니다.
