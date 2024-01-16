07111d4d8693076f05381645f636a396b322f881
sscoderati <chungup5495@gmail.com>
2024-01-16 22:36:06 +0900

    Fix: 인증 헤더 구성 로직 인터셉터로 재이관

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -6,9 +6,16 @@ export const axiosAPI = axios.create({
     process.env.NODE_ENV === "development"
       ? "http://localhost:5173"
       : import.meta.env.VITE_BASE_URL,
-  headers: {
-    Authorization: `${useAuthStore.getState().authTokens?.accessToken}`,
-  },
 });
+
+axiosAPI.interceptors.request.use(
+  (config) => {
+    config.headers.Authorization = `${useAuthStore.getState().authTokens?.accessToken}`;
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
 
 // TODO: axiosAPI.interceptors.response.use()를 사용하여 refreshToken이 만료되었을 때, accessToken을 재발급 받는 로직을 구현해야 합니다.
