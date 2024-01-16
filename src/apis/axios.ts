b0fa507a45af9d60f5e03f90575388bb1d936108
sscoderati <chungup5495@gmail.com>
2024-01-16 22:31:39 +0900

    Chore: axios 인터셉터 비활성화 및 인스턴스 생성 시 auth 헤더 구성토록 변경

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -6,20 +6,9 @@ export const axiosAPI = axios.create({
     process.env.NODE_ENV === "development"
       ? "http://localhost:5173"
       : import.meta.env.VITE_BASE_URL,
-  // headers: {
-  //   Authorization: `${tokens?.accessToken}`,
-  // },
+  headers: {
+    Authorization: `${useAuthStore.getState().authTokens?.accessToken}`,
+  },
 });
-
-axiosAPI.interceptors.request.use(
-  (config) => {
-    console.log(useAuthStore.getState().authTokens?.accessToken);
-    config.headers.Authorization = `${useAuthStore.getState().authTokens?.accessToken}`;
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  },
-);
 
 // TODO: axiosAPI.interceptors.response.use()를 사용하여 refreshToken이 만료되었을 때, accessToken을 재발급 받는 로직을 구현해야 합니다.
