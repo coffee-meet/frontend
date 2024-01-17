56ed6d26861b764ec0944887ca777b8d5f75ba22
sscoderati <chungup5495@gmail.com>
2024-01-18 01:43:39 +0900

    Remove: 필요없는 파일 삭제
    - no usuage인 경우
    - 주석처리로라도 남아있지 않은 경우

--- src/apis/test.ts
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-export const testWithBtn = async (nickname: string) => {
-  return axios.get(`https://api.coffee-meet.site/api/v1/users/duplicate?nickname=${nickname}`);
-};
