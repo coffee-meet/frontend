3499dfcb8c209ad2a67342201d63ac7dc65dfc31
sscoderati <chungup5495@gmail.com>
2024-01-27 23:57:13 +0900

    Feat: Toast Provider 설정

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -1,11 +1,21 @@
 import { Outlet } from "react-router-dom";
 import styled from "@emotion/styled";
+import { Toaster } from "sonner";
 import Modal from "@/components/common/Modal";
 import { theme } from "@/styles/theme";
+import useThemeStore from "@/store/ThemeStore.tsx";
 
 const Layout = () => {
+  const isDarkMode = useThemeStore((state) => state.isDarkMode);
+
   return (
     <MainContainer>
+      <Toaster
+        duration={2000}
+        position={"top-center"}
+        theme={isDarkMode ? "dark" : "light"}
+        richColors
+      />
       <Modal />
       <Outlet />
     </MainContainer>
